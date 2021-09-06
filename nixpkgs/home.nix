ff4f1e8ac5fd54ec8903d34142c62af7e881d2e8
Tobias Bales <hi@tobiasbales.dev>
2021-09-06 11:03:28 +0200

    Track initial home manager config

---
+++ nixpkgs/home.nix
@@ -0,0 +1,177 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "nixos";
+  home.homeDirectory = "/home/nixos";
+
+  home.packages = with pkgs; [
+    diff-so-fancy
+    git-interactive-rebase-tool
+    exa
+    ripgrep
+    tldr
+  ];
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      package = pkgs.fira-code;
+      name = "Fira Code";
+      size = 12;
+    };
+    settings = {
+      foreground = "#ebdbb2";
+      background = "#272727";
+      selection_foreground = "#655b53";
+      selection_background = "#ebdbb2";
+      url_color = "#d65c0d";
+      # black
+      color0 = "#272727";
+      color8 = "#928373";
+      # red
+      color1 = "#cc231c";
+      color9 = "#fb4833";
+      # green
+      color2 = "#989719";
+      color10 = "#b8ba25";
+      # yellow
+      color3 = "#d79920";
+      color11 = "#fabc2e";
+      # blue
+      color4 = "#448488";
+      color12 = "#83a597";
+      # magenta
+      color5 = "#b16185";
+      color13 = "#d3859a";
+      # cyan
+      color6 = "#689d69";
+      color14 = "#8ec07b";
+      # white
+      color7 = "#a89983";
+      color15 = "#ebdbb2";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Tobias Bales";
+    userEmail = "hi@tobiasbales.dev";
+    aliases = {
+    };
+    extraConfig = {
+      color = {
+        branch = "auto";
+	diff = "auto";
+	interactive = "auto";
+	status = "auto";
+	ui = "auto";
+      };
+      branch = {
+        autosetupmerge = "true";
+      };
+      core = {
+        editor = "nvim";
+	exludesfile = "~/.gitignore";
+	autocrlf = "input";
+	pager = "diff-so-fancy | less --tabs=4 -RFX";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      pull = {
+        rebase = "true";
+      };
+      push = {
+        default = "simple";
+	followTags = "true";
+      };
+      rebase = {
+        autosquash = "true";
+      };
+      rerere = {
+        enabled = "true";
+      };
+      sequence = {
+        editor = "interactive-rebase-tool";
+      };
+      submodule = {
+        recurse = "true";
+      };
+      status = {
+        showUntrackedFiles = "all";
+      };
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+    shortcut = "a";
+    baseIndex = 1;
+    keyMode = "vi";
+    sensibleOnTop = true;
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.pain-control
+      tmuxPlugins.gruvbox
+    ];
+    extraConfig = "
+      set-option -g mouse on
+      set-option -g allow-rename off
+      set-option -g visual-activity off
+      set-option -g visual-bell off
+      set-option -g visual-silence off
+      set-window-option -g monitor-activity off
+      set-option -g bell-action none
+      set-option -g renumber-windows on
+    ";
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    shellAliases = {
+      cat = "bat";
+      la = "exa --all --long";
+      ll = "exa --long";
+      ls = "exa";
+      man = "tldr";
+      ta = "tmux attach -t ";
+      tl = "tmux ls";
+      tn = "tmux new -s ";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "asdf"
+        "git"
+	"ripgrep"
+        "sudo"
+	"z"
+      ];
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+}
