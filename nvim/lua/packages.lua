d549522b90d8aa2c86a4fbcefc7e68eb261b64a4
Tobias Bales <tobias.bales@shopify.com>
2022-05-24 13:37:12 +0200

    feat: load packer on first nvim start

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -1,9 +1,12 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 local packer_bootstrap
+
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
+
+vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- integrate navigation between tmux and vim using ctrl+hjkl
