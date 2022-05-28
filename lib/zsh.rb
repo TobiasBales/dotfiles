4197a65c9f68cf5837ee1bed49b27445c0b91c2b
Tobias Bales <git@tobiasbales.dev>
2022-05-28 19:50:34 +0200

    feat: add personal zsh additions

--- lib/zsh.rb
+++ lib/zsh.rb
@@ -6,5 +6,6 @@ class Zsh < Base
     FetchFile.new(url: "https://git.io/antigen", target: "~/.antigen.zsh").run
     Link.new(source: "zsh/starship.toml", target: "~/.config/starship.toml").run
     ExecuteFile.new(url: "https://starship.rs/install.sh", executable: "starship").run
+    Link.new(source: "zsh/personal.zsh", target: "~/.additional.zsh").run if personal?
   end
 end
---
+++ zsh/personal.zsh
@@ -0,0 +1,2 @@
+export CONSUL_ADDR=http://node01.local:8500
+export NOMAD_ADDR=http://node01.local:4646
