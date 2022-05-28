adc100b6f6609fde85a8d9bc4991576b8bf79e86
Tobias Bales <git@tobiasbales.dev>
2022-05-28 14:52:50 +0200

    feat: allow re-running commands with LastRun based on config file changes

--- lib/last_run.rb
+++ lib/last_run.rb
@@ -5,9 +5,9 @@ class LastRun < Base
     @@instance ||= LastRun.new
   end
 
-  def run_if_needed(command, sub_directory: nil, &blk)
+  def run_if_needed(command, manifest: nil, sub_directory: nil, &blk)
     debug("Checking if #{command} needs to be run")
-    if was_run_recently?
+    if was_run_recently? && !manifest_changed?(sub_directory, manifest)
       debug("Was run recently, skipping")
       return
     end
@@ -23,6 +23,12 @@ class LastRun < Base
   end
 
   private
+
+  def manifest_changed?(sub_directory, manifest)
+    return false if manifest.nil?
+
+    File.mtime(File.join(directory(sub_directory: sub_directory), manifest)).to_i > last_run_time
+  end
 
   def was_run_recently?
     time_since_last_run = Time.now.to_i - last_run_time
