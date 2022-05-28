02bfede8e4b4fdeabad6dec9e33f1c45fab3f73b
Tobias Bales <git@tobiasbales.dev>
2022-05-28 19:19:21 +0200

    fix: install neovim via snap on non spin linux

--- lib/neo_vim.rb
+++ lib/neo_vim.rb
@@ -7,8 +7,7 @@ class NeoVim < Base
     debug("")
     debug("Setting up neovim config")
     Link.new(source: "nvim", target: "~/.config/nvim").run
-    Script.new(file: "install_neovim").run if linux? && spin?
-    @last_run.run_if_needed("sudo apt-get install -y neovim") if linux? && !spin?
+    Script.new(file: "install_neovim").run if linux?
     LuaPackage.new(name: "luacheck").run
   end
 end
