7512173d64f4b46a0bdf75dc11103b897396939a
Tobias Bales <git@tobiasbales.dev>
2022-05-28 14:53:15 +0200

    feat: re-run brew bundle when Brewfile changes

--- lib/homebrew.rb
+++ lib/homebrew.rb
@@ -11,8 +11,8 @@ class Homebrew < Base
           return
         end
 
-        @last_run.run_if_needed("brew bundle")
-        @last_run.run_if_needed("brew bundle", sub_directory: "personal") if personal?
-        @last_run.run_if_needed("brew bundle", sub_directory: "work") if work?
+        @last_run.run_if_needed("brew bundle", manifest: "Brewfile")
+        @last_run.run_if_needed("brew bundle", manifest: "Brewfile", sub_directory: "personal") if personal?
+        @last_run.run_if_needed("brew bundle", manifest: "Brewfile", sub_directory: "work") if work?
     end
 end
