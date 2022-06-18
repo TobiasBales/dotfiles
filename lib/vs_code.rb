d19a65d7fb2e2f4b5d1a84fa39b80bc96bb9ed63
Tobias Bales <git@tobiasbales.dev>
2022-06-18 23:05:49 +0200

    feat: add basic vs code settings

---
+++ lib/vs_code.rb
@@ -0,0 +1,12 @@
+# typed: strict
+# frozen_string_literal: true
+
+class VSCode < Base
+  sig { override.void }
+  def run
+    return unless macos?
+
+    Link.new(source: "vscode/settings.json",
+             target: "~/Library/Application Support/Code/User/settings.json").run
+  end
+end
