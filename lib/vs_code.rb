6d41aa1663c9814a9854710381691858121ae1bf
Tobias Bales <git@tobiasbales.dev>
2022-06-19 08:48:19 +0200

    feat: byesig increase opacity to 0.3

--- lib/vs_code.rb
+++ lib/vs_code.rb
@@ -8,5 +8,7 @@ class VSCode < Base
 
     Link.new(source: "vscode/settings.json",
              target: "~/Library/Application Support/Code/User/settings.json").run
+    Link.new(source: "vscode/keybindings.json",
+             target: "~/Library/Application Support/Code/User/keybindings.json").run
   end
 end
