37796fd537052544c282b29838288183b0808d40
Tobias Bales <tobias.bales@shopify.com>
2022-06-27 10:48:13 +0200

    feat: enable homebrew auto updates

--- lib/homebrew.rb
+++ lib/homebrew.rb
@@ -23,5 +23,19 @@ class Homebrew < Base
     @last_run.run_if_needed("brew bundle", manifest: "Brewfile")
     @last_run.run_if_needed("brew bundle", manifest: "Brewfile", sub_directory: "personal") if personal?
     @last_run.run_if_needed("brew bundle", manifest: "Brewfile", sub_directory: "work") if work?
+
+
+    enable_auto_updates
+  end
+
+  private
+
+  sig { void }
+  def enable_auto_updates
+    return if manifest_exists?("brew_auto_update")
+
+    `brew autoupdate start --upgrade --cleanup`
+
+    create_manifest("brew_auto_update")
   end
 end
