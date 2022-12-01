ab8a6839d9503428282ccce39c9f1dd22d6c1b4a
Tobias Bales <tobias.bales@shopify.com>
2022-12-01 10:37:27 +0100

    chore: split git monitor between macos and linux

--- lib/git.rb
+++ lib/git.rb
@@ -10,10 +10,17 @@ class Git < Base
     debug("Setting up git config")
 
     Link.new(source: "git/config", target: "~/.gitconfig").run
-    Link.new(
-      source: "git/fsmonitor-watchman",
-      target: "~/.fsmonitor-watchman"
-    ).run
+    if linux?
+      Link.new(
+        source: "git/fsmonitor-watchman",
+        target: "~/.fsmonitor-watchman"
+      ).run
+      Link.new(source: "git/config.linux", target: "~/.gitconfig.linux").run
+    end
+
+    if macos?
+      Link.new(source: "git/config.macos", target: "~/.gitconfig.macos").run
+    end
 
     if work?
       Link.new(source: "git/config.shopify", target: "~/.gitconfig.shopify").run
