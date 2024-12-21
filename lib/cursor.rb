6b06528a113794adb5da688b0685ed446941ad0d
Tobias Bales <hi@tobiasbales.com>
2024-12-21 18:52:48 +0100

    Fix cursor config and drop a lot of unused/unneeded things

--- lib/cursor.rb
+++ lib/cursor.rb
@@ -6,10 +6,10 @@ class Cursor < Base
   def run
     return unless macos?
     Link.new(source: "cursor/settings.json",
-             target: "~/Library/Application Support/Cursor/settings.json").run
+             target: "~/Library/Application Support/Cursor/User/settings.json").run
     Link.new(source: "cursor/keybindings.json",
-             target: "~/Library/Application Support/Cursor/keybindings.json").run
+             target: "~/Library/Application Support/Cursor/User/keybindings.json").run
     Link.new(source: "cursor/snippets",
-             target: "~/Library/Application Support/Cursor/snippets").run
+             target: "~/Library/Application Support/Cursor/User/snippets").run
   end
 end
